c1fd81cf9d7435acd8f221dd81b8f26fb2551480
ahmadsyahrizar <ahmad.ramadhan@tokopedia.com>
2023-04-03 20:00:46 +0700

    style(carRental): fix css

--- project/src/components/Hero/index.js
+++ project/src/components/Hero/index.js
@@ -1,31 +1,42 @@
-import React from 'react'
-import './style.css'
-import carImg from '../Hero/carImg.png'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import "./style.css";
+import carImg from "../Hero/carImg.png";
+import { NavLink } from "react-router-dom";
 
 const Hero = () => {
-    return (
-        <div id="hero-section" class="bg-light-purpple" >
-            <div class="container">
-                <div class="row">
-                    <div class="col-6 left-hero pr-4 d-flex align-items-center">
-                            <h1 class="heading-style" style={{ color: "black", fontweight: "bold" }}> Sewa dan Rental Mobil Terbaik di kawasan Bandar Lampung</h1><br />
-                            <h6>Selamat datang di Binar Car Rental, Kami menyediakan mobil kualitas terbaik dengan harga terjangkau. Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam.</h6>
-                            <br />
+  return (
+    <div class="container-fluid padding-custom">
+      <div class="row">
+        <div className="col">
+          <h1>Sewa & Rental Mobil Terbaik di kawasan (Lokasimu)</h1>
+          <p>
+            Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas
+            terbaik dengan harga terjangkau. Selalu siap melayani kebutuhanmu
+            untuk sewa mobil selama 24 jam.
+          </p>
 
-                            <button OnClick={() => ('/cari-mobil')} type="button" className='btn btn-success mt-'>
-                            <a>Mulai Sewa Mobil</a>
-                            </button>
-                      </div>
-                </div>
-                <div class="col-lg-6 right-hero"> </div>
-                <div class="car-style">
-                </div>
+          <a
+            name=""
+            id=""
+            class="btn btn-primary"
+            style={{ background: "#5CB85F", border: "none" }}
+            href="/cari-mobil"
+            role="button"
+          >
+            Mulai Sewa Mobil
+          </a>
+        </div>
 
-                <img class="car img-fluid" src={carImg} alt="car" style = {{width: "50%" }}/>
-        </div >  
-        </div>  
-    )
-}
+        <div className="col">
+          <div className="car-style" style={{ position: "relative" }}>
+            <div className="car-img-wrapper">
+              <img src={carImg} width="600px" height={"auto"} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
